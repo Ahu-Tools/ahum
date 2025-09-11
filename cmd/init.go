645357f00e59a21cd59c36d530c06aad1bc85436
Sina Sadeghi <sina.sadeghi83@gmail.com>
2025-09-11 13:07:30 +0330

    :hammer: Add arguments check for init command

--- cmd/init.go
+++ cmd/init.go
@@ -4,7 +4,9 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -14,6 +16,7 @@ var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Initialise an Ahu project",
 	Long:  `Create folders and go files related to the main architecture of the Ahu project`,
+	Args:  cobra.MatchAll(cobra.RangeArgs(1, 3), checkPortRange),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("init called")
 	},
@@ -31,4 +34,28 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func checkPortRange(cmd *cobra.Command, args []string) error {
+	var portStr string
+
+	switch len(args) {
+	case 2:
+		portStr = args[1]
+	case 3:
+		portStr = args[2]
+	default:
+		return nil
+	}
+
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		return errors.New("port must be a positive integer number")
+	}
+
+	if port > 65535 || port <= 0 {
+		return errors.New("provided port is not in the valid range")
+	}
+
+	return nil
 }
