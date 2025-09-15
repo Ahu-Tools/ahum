76fde202ebb2c7127d5073e7952d8a70bd653a39
Sina Sadeghi <sina.sadeghi83@gmail.com>
2025-09-15 16:57:52 +0330

    Add initial help to ahum main cobra app

--- cmd/root.go
+++ cmd/root.go
@@ -16,16 +16,8 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "ahum",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "A project management wizard for Ahu services",
+	Long:  `This wizard will help you to create and manage an Ahu project. You can choose modules you need for your project and your porject will be shipped with the modules you need.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -45,10 +37,6 @@ func init() {
 	// will be global for your application.
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ahum.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
