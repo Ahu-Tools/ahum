be2570d9127dddfec3d813554ab9f3afade9de5d
Sina Sadeghi <sina.sadeghi83@gmail.com>
2025-09-21 19:47:22 +0330

    :hammer: CONNECT/Config: Add and Register connect Loader

--- pkg/connect/config.go
+++ pkg/connect/config.go
@@ -1,5 +1,10 @@
 package connect
 
+import (
+	"github.com/Ahu-Tools/AhuM/pkg/config"
+	"github.com/Ahu-Tools/AhuM/pkg/project"
+)
+
 const Name = "connect"
 
 // We want to implement project.Edge for Connect
@@ -17,4 +22,22 @@ func (g *Connect) JsonConfig() any {
 
 func (g *Connect) Load() (string, error) {
 	return "", nil
+}
+
+func init() {
+	project.RegisterEdgeLoader(Name, Loader)
+}
+
+func Loader(pj project.Project, cfgGroup string) (project.Edge, error) {
+	genGuide, err := pj.GetConfigGenGuide()
+	if err != nil {
+		return nil, err
+	}
+
+	cfg, err := config.LoadConfigByGroup[ConnectConfig](cfgGroup, &Connect{}, *genGuide)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewConnect(&pj.Info, *cfg), nil
 }
