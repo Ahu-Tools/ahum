e7829227fea692aa0e3d1ebda88701caf88e4f93
Sina Sadeghi <sina.sadeghi83@gmail.com>
2025-09-21 19:38:02 +0330

    :hammer: :boom: GIN/Config: Define Loader and register it in init()

--- pkg/gin/config.go
+++ pkg/gin/config.go
@@ -1,5 +1,10 @@
 package gin
 
+import (
+	"github.com/Ahu-Tools/AhuM/pkg/config"
+	"github.com/Ahu-Tools/AhuM/pkg/project"
+)
+
 // We want to implement project.Edge for Gin
 func (g *Gin) Name() string {
 	return Name
@@ -15,4 +20,22 @@ func (g *Gin) JsonConfig() any {
 
 func (g *Gin) Load() (string, error) {
 	return "", nil
+}
+
+func init() {
+	project.RegisterEdgeLoader(Name, Loader)
+}
+
+func Loader(pj project.Project, cfgGroup string) (project.Edge, error) {
+	genGuide, err := pj.GetConfigGenGuide()
+	if err != nil {
+		return nil, err
+	}
+
+	ginConfig, err := config.LoadConfigByGroup[GinConfig](cfgGroup, &Gin{}, *genGuide)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewGin(&pj.Info, *ginConfig), nil
 }
