38bbcc9064314e466de09b8e7c563a577b5e2c53
Sina Sadeghi <sina.sadeghi83@gmail.com>
2025-09-21 19:45:00 +0330

    :art: CONNECT/Config: Use Name constant for Name()

--- pkg/connect/config.go
+++ pkg/connect/config.go
@@ -1,8 +1,10 @@
 package connect
 
+const Name = "connect"
+
 // We want to implement project.Edge for Connect
 func (g *Connect) Name() string {
-	return "connect"
+	return Name
 }
 
 func (g *Connect) Pkgs() ([]string, error) {
