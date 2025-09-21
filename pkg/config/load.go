2fdef95eac225f229147955862d9dac56cfeca99
Sina Sadeghi <sina.sadeghi83@gmail.com>
2025-09-21 19:12:49 +0330

    :hammer: Add the function of adding a new config to a created project

--- pkg/config/load.go
+++ pkg/config/load.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"path/filepath"
+	"strings"
 
 	gen "github.com/Ahu-Tools/AhuM/pkg/generation"
 	"github.com/Ahu-Tools/AhuM/pkg/util"
@@ -24,4 +25,35 @@ func LoadConfig[T any](genGuide gen.Guide, name string) (*T, error) {
 
 	config, err := util.LoadJSONPathToStruct[T](configPath, jsonPath)
 	return config, err
+}
+
+func AddConfigByGroup(group string, cfg Configurable, genGuide gen.Guide) error {
+	configPath := filepath.Join(genGuide.RootPath, "config.json")
+	err := util.AddElementToJSON(configPath, group, cfg.Name(), cfg.JsonConfig())
+	if err != nil {
+		return err
+	}
+
+	goCfgPath := filepath.Join(genGuide.RootPath, "config.go")
+	load, err := cfg.Load()
+	if err != nil {
+		return err
+	}
+	load = fmt.Sprintf(`//@ahum:%s.load
+	%s
+	//@ahum:end.%s.load`, cfg.Name(), load, cfg.Name())
+
+	pkgs, err := cfg.Pkgs()
+	if err != nil {
+		return err
+	}
+	pkgs = util.Map(pkgs, func(pkg string) string {
+		return fmt.Sprintf(`"%s"`, pkg)
+	})
+	insertions := map[string]string{
+		"imports":                          strings.Join(pkgs, "\n"),
+		fmt.Sprintf("end.%s.group", group): load,
+	}
+
+	return util.ModifyCodeByMarkersFile(goCfgPath, insertions, genGuide.FilePerms)
 }
