cf3f6863511b918316051d77121563e5ccb55dd6
Sina Sadeghi <sina.sadeghi83@gmail.com>
2025-10-01 17:59:14 +0330

    :hammer: ASYNQ/Edge: Implement add task handler

--- pkg/asynq/edge/edge.go
+++ pkg/asynq/edge/edge.go
@@ -8,6 +8,7 @@ import (
 	gen "github.com/Ahu-Tools/ahum/pkg/generation"
 	"github.com/Ahu-Tools/ahum/pkg/project"
 	"github.com/Ahu-Tools/ahum/pkg/util"
+	"github.com/iancoleman/strcase"
 )
 
 type Asynq struct {
@@ -34,7 +35,44 @@ func (ae *Asynq) Generate(statusChan chan string, genGuide gen.Guide) error {
 		return err
 	}
 
+	err = ae.AddTaskHandler("world", "hello", "v1", genGuide)
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func (ae *Asynq) AddTaskHandler(taskName, modName, verName string, genGuide gen.Guide) error {
+	taskName = strcase.ToCamel(taskName)
+	verName = strings.ToLower(verName)
+	modName = strings.ToLower(modName)
+
+	path := filepath.Join(genGuide.RootPath, modName, verName, modName+".go")
+	data := map[string]any{
+		"TaskName":      taskName,
+		"TaskNameKebab": strcase.ToKebab(taskName),
+	}
+	payload, _ := util.ParseTemplateString("asynq/edge/payload_struct.go.tpl", data)
+	handler, _ := util.ParseTemplateString("asynq/edge/handle_func.go.tpl", data)
+	insertions := map[string]string{
+		"payloads": payload,
+		"handlers": handler,
+	}
+
+	err := util.ModifyCodeByMarkersFile(path, insertions, genGuide.FilePerms)
+	if err != nil {
+		return err
+	}
+
+	path = filepath.Join(genGuide.RootPath, modName, verName, "registrar.go")
+	taskType, _ := util.ParseTemplateString("asynq/edge/task_type.go.tpl", data)
+	taskRegister, _ := util.ParseTemplateString("asynq/edge/task_register.go.tpl", data)
+	insertions = map[string]string{
+		"types":     taskType,
+		"registers": taskRegister,
+	}
+	return util.ModifyCodeByMarkersFile(path, insertions, genGuide.FilePerms)
 }
 
 func (ae *Asynq) AddModule(verName, modName string, genGuide gen.Guide) error {
